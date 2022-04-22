e2d8ac95d6dc253f1cf3cd678bcf56aafa9c9ac3
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2022-04-22 13:23:28 +0100

    Add files via upload

---
+++ AnimalRunner.py
@@ -0,0 +1,30 @@
+from Animal import Cat
+from Animal import Bat
+from Animal import Penguin
+from Animal import Pigeon
+
+obj = Cat()
+print(obj.name())
+print(obj.eat())
+print(obj.reproduce())
+
+obj1 = Bat()
+print(obj1.name())
+print(obj1.reproduce())
+print(obj1.eat())
+
+
+obj2 = Penguin()
+print(obj2.name())
+print(obj2.reproduce())
+print(obj2.eat())
+
+
+obj3 = Pigeon()
+print(obj3.name())
+print(obj3.reproduce())
+print(obj3.eat())
+print(obj3.movement())
+
+
+
